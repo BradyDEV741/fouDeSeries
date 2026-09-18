8951a54ef35edc93bc358d17aec625d76b61b3b0
Brady <bradyrandria10@gmail.com>
2026-09-18 10:49:25 +0200

    test: Ajout d'une condition dans le fichier twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ final class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'Bienvenue sur Fou de Séries',
+            'controller_name' => 'Bienvenue sur fou de Séries',
         ]);
     }
 }
